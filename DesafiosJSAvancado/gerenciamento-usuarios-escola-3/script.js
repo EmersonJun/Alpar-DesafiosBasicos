c50ee3c775544145adf8adae0dc0607acfba9759
EmersonJun <EmersonnJunior2006@gmail.com>
2026-03-29 22:46:33 -0300

    correcoes JSAvancado modulo 6

--- DesafiosJSAvancado/gerenciamento-usuarios-escola-3/script.js
+++ DesafiosJSAvancado/gerenciamento-usuarios-escola-3/script.js
@@ -1,11 +1,9 @@
 const app = angular.module("userApp", []);
 
-// CONTROLLER PAI
 app.controller("AppController", function ($scope) {
     $scope.mensagem = "Sistema de Usuários";
 });
 
-// CONTROLLER FILHO
 app.controller("ListaController", function ($scope) {
 
     $scope.usuarios = [
